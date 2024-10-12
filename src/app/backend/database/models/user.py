170deeeed7be086a9d01f89825ce8e50ff4e0d48
ahmedennaifer <ahmedennaifer00@gmail.com>
2024-10-12 20:08:38 +0200

    [FIX] - removed unused imports

--- src/app/backend/database/models/user.py
+++ src/app/backend/database/models/user.py
@@ -1,7 +1,5 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from sqlalchemy.types import DateTime
-from datetime import datetime
 from ..db import Base
 
 
