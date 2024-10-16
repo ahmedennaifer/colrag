868fdc30ae1dbe4ca02a6caa3b50058577658b3b
ahmedennaifer <ahmedennaifer00@gmail.com>
2024-10-16 10:38:29 +0200

    [FIX] - fixed project structure + imports

--- src/app/backend/main.py
+++ src/app/backend/main.py
@@ -2,14 +2,20 @@ from fastapi import FastAPI, Depends, HTTPException, status, UploadFile, Form
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
-from jose import JWTError, jwt
 from pydantic import BaseModel, EmailStr
 from datetime import datetime, timedelta
-from typing import Optional
 
-from .database.db import get_db
-from .database.models.user import User
-from .database.models.document import Document
+from src.app.backend.database.db import get_db
+from src.app.backend.database.models.user import User
+from src.app.backend.database.models.document import Document
+
+from .auth.utils import (
+    verify_password,
+    get_current_user,
+    create_access_token,
+    get_password_hash,
+)
+
 
 import logging
 import colorlog
@@ -35,50 +41,11 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 SECRET_KEY = "test"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30*2*24
+ACCESS_TOKEN_EXPIRE_MINUTES = 30 * 2 * 24
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
-
-
-def get_current_user(
-    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
-):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id: int = payload.get("sub")
-        if user_id is None:
-            raise credentials_exception
-        user = db.query(User).filter(User.id == user_id).first()
-        if user is None:
-            raise credentials_exception
-    except JWTError:
-        raise credentials_exception
-    return user
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
 class UserReq(BaseModel):
