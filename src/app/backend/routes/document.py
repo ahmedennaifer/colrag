c6062ed4e8286abd3437b3308f8177aaf758481f
ahmedennaifer <ahmedennaifer00@gmail.com>
2024-11-03 11:14:43 +0100

    bug fix, attribute doc to workspace correctly (private and public)

--- src/app/backend/routes/document.py
+++ src/app/backend/routes/document.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Depends, UploadFile, HTTPException
-from sqlalchemy import or_
-from sqlalchemy.orm import Session, joinedload
+from typing import Optional
+from fastapi import APIRouter, Depends, UploadFile, HTTPException, Form
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from sqlalchemy import or_, and_
 from datetime import datetime
 
 from src.app.backend.database.models.document import Document
@@ -13,25 +15,51 @@ from src.app.backend.auth.utils import logger
 router = APIRouter()
 
 
+class DocumentWorkspaceProperties(BaseModel):
+    workspace_name: str = Form()
+    workspace_id: Optional[int] = Form(None)
+
+
 @router.post("/send_document")
 async def upload_document(
     doc: UploadFile,
     current_user: User = Depends(get_current_user),
     db: Session = Depends(get_db),
-    workspace_id: int = None,
+    properties: DocumentWorkspaceProperties = Depends(),
 ):
+    workspace = None
+    logger.info(properties.workspace_name)
+    if properties.workspace_id:
+        workspace = (
+            db.query(Workspace).filter(Workspace.id == properties.workspace_id).first()
+        )
+    elif properties.workspace_name:
+        workspace = (
+            db.query(Workspace)
+            .filter(Workspace.name == properties.workspace_name)
+            .first()
+        )
+
+    else:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Workspace with name {properties.workspace_name} not found!",
+        )
+
     document = Document(
         filename=doc.filename,
         file_path=f"dump/{doc.filename}",
         file_type=f"{doc.filename[-3:]}",
         uploaded_at=datetime.now(),
         user_id=current_user.id,
-        workspace_id=1,  # TODO: this
+        workspace_id=workspace.id if workspace else None,
     )
     try:
         db.add(document)
         db.commit()
-        logger.info(f"Document uploaded by {current_user.email}")
+        logger.info(
+            f"Document uploaded by {current_user.email} into workspace: {properties.workspace_name}"
+        )
         return {"message": "Document uploaded successfully", "document": doc.filename}
     except Exception as e:
         db.rollback()
@@ -47,13 +75,34 @@ async def get_all_docs(
 ):
     documents = (
         db.query(Document)
-        .filter(Workspace.privacy == "public")
-        .filter(Workspace.creator_id == user.id)
+        .join(Workspace, Document.workspace_id == Workspace.id)
+        .filter(or_(Workspace.privacy == "public", Workspace.creator_id == user.id))
         .all()
     )
     if documents:
         return {
-            "documents": [{"document": document.filename} for document in documents]
+            "documents": [
+                {
+                    "document name": document.filename,
+                    "document id": document.id,
+                    "author": document.owner.username,
+                    "workspace_name": (
+                        document.workspace.name if documents else "No docs"
+                    ),
+                }
+                for document in documents
+            ]
         }
     else:
         raise HTTPException(status_code=404, detail="No documents found!")
+
+
+@router.get("/get_document/{id}")
+async def get_doc_by_id(
+    id: int, user: Session = Depends(get_current_user), db: Session = Depends(get_db)
+):
+    document = db.query(Document).filter(User.id == user.id, Document.id == id).first()
+    if document:
+        return document
+    else:
+        raise HTTPException(status_code=404, details="No document with id: {id} found ")
