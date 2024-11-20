aedfa58367cab67acfefa99d27483f780afae0aa
ahmedennaifer <ahmedennaifer00@gmail.com>
2024-11-20 15:38:24 +0100

    feat: add qdrant integration, with multiple collection. Bug: problem with collection creation

--- src/app/backend/reddit.py
+++ src/app/backend/reddit.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 import praw
-from qdrant_client import QdrantClient, models
+from qdrant_client import QdrantClient
 from datetime import datetime
 
 load_dotenv()
@@ -18,10 +18,19 @@ sub = reddit.subreddit("Python")
 all_submissions = []
 post_id = "1gu7g70"
 submission = reddit.submission(id=post_id)
+comments = submission.comments.list()
+comment = reddit.comment(comments[0].body)
+print(comment)
+
+
 
 client = QdrantClient(url="http://localhost:6333")
 
-client.create_collection(
-    collection_name="Reddit collection test 2",
-    vectors_config=models.VectorParams(size=100, distance=models.Distance.COSINE),
-)
+# client.create_collection(
+#      collection_name="Reddit collection test 2",
+#      vectors_config=models.VectorParams(size=100, distance=models.Distance.COSINE),
+# )
+
+# client.add(collection_name="Reddit Collection test", documents=comment)
+client.get_collection(collection_name="Reddit Collection test 2")
+print(client)
