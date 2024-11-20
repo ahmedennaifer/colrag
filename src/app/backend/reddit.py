b392569caf1e97750893d07125ea5fc5a03d5ba2
ahmedennaifer <ahmedennaifer00@gmail.com>
2024-11-20 13:51:24 +0100

    added qdrant integration

--- src/app/backend/reddit.py
+++ src/app/backend/reddit.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import os
 import praw
+from qdrant_client import QdrantClient, models
+from datetime import datetime
 
 load_dotenv()
 
@@ -17,4 +19,9 @@ all_submissions = []
 post_id = "1gu7g70"
 submission = reddit.submission(id=post_id)
 
-print(submission.title)
+client = QdrantClient(url="http://localhost:6333")
+
+client.create_collection(
+    collection_name="Reddit collection test 2",
+    vectors_config=models.VectorParams(size=100, distance=models.Distance.COSINE),
+)
