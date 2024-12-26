61e6dbcf9358fdabd7477631a13d3fee7d0777d9
ahmedennaifer <ahmedennaifer00@gmail.com>
2024-12-26 16:18:29 +0100

    misc: cleaned useless chatgpt comments

--- start.sh
+++ start.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-set -e  # Exit immediately if a command exits with a non-zero status
-set -x  # Print commands and their arguments as they are executed
-
-# Debugging statements
+set -e  
+set -x  
 echo "Current working directory: $(pwd)"
 
-# Create the alembic/versions directory
 echo "Creating alembic/versions directory..."
 mkdir -p alembic/versions
 
-# List contents to verify
 ls -l alembic
 ls -l alembic/versions
 
