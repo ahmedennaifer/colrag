b73bf84dc154a74b806193a449533fd70625b5ce
ahmedennaifer <ahmedennaifer00@gmail.com>
2024-12-31 20:41:28 +0100

    feat: added RDS and start.sh changes

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,11 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 
+
 COPY . .
 
 COPY start.sh /start.sh
+RUN apt-get update && apt-get install -y postgresql-client
 RUN chmod +x /start.sh
 
 EXPOSE 8000
--- start.sh
+++ start.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
-set -e  
-set -x  
+
+set -e
+set -x
+
+
+source .env
+
 echo "Current working directory: $(pwd)"
+
 
 echo "Creating alembic/versions directory..."
 mkdir -p alembic/versions
-
 ls -l alembic
 ls -l alembic/versions
 
 
-echo "Running alembic upgrade..."
+echo "Initializing fresh Alembic setup..."
 
-alembic revision --autogenerate -m "initial migration"
+rm -f alembic/versions/*
+
+
+PGPASSWORD=${DB_PASSWORD} psql -h ${DB_HOST} -U ${DB_USER} -d ${DB_NAME} -c "DROP TABLE IF EXISTS alembic_version;"
+
+
+echo "Creating new initial migration..."
+alembic revision --autogenerate -m "fresh_start"
+
+
+echo "Applying migration..."
 alembic upgrade head
 
 echo "Starting the application..."
